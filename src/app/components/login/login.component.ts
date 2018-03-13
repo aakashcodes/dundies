3a110b9f1f2d596a043f166a4f8a21c01bae4936
Aakash Patel <aakashpatel@Aakashs-MacBook-Pro-2.local>
2018-03-13 14:50:04 -0400

    Just do it

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,43 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup } from "@angular/forms";
 import { ApiService } from "../../services/api.service";
 import { Router } from "@angular/router";
 
-
-@Component ( {
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: [ './login.component.css' ]
-} )
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
+})
 export class LoginComponent implements OnInit {
-  email: string = "";
-  errorMsg: string = "";
+  email = "";
+  errorMsg = "";
 
-  constructor ( private api: ApiService, private router: Router ) {
+  constructor(private api: ApiService, private router: Router) {}
 
-  }
+  ngOnInit() {}
 
-  ngOnInit () {
-  }
-
-  verifyEmail (): void {
-    this.errorMsg='';
-    if (this.email != '') {
-      this.api.getUser ().subscribe ( ( data ) => {
-        data.forEach ( user => {
-          if (user.email.toLowerCase () === this.email.toLowerCase ()) {
-            localStorage.setItem ( 'loggedInUser', JSON.stringify ( user ) );
-            this.router.navigate ( [ '/profile/1' ] );
-          }
-        } );
-          this.errorMsg="Email address not found.";
-      }, ( error ) => {
-        console.log(error);
-        this.errorMsg="There was a error while fetching user details. Please try again."
-      } )
+  verifyEmail(): void {
+    this.errorMsg = "";
+    if (this.email !== "") {
+      this.api.getUser().subscribe(
+        data => {
+          data.forEach(user => {
+            if (user.email.toLowerCase() === this.email.toLowerCase()) {
+              localStorage.setItem("loggedInUser", JSON.stringify(user));
+              this.router.navigate(["/profile/1"]);
+            }
+          });
+          this.errorMsg = "Email address not found.";
+        },
+        error => {
+          console.log(error);
+          this.errorMsg =
+            "There was a error while fetching user details. Please try again.";
+        }
+      );
     } else {
-      this.errorMsg= "Email is required.";
+      this.errorMsg = "Email is required.";
     }
   }
-
 }
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,50 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService, userDetails, userPost, PostComments } from "../../services/api.service";
+import { Component, OnInit } from "@angular/core";
+import {
+  ApiService,
+  userDetails,
+  userPost,
+  PostComments
+} from "../../services/api.service";
 
-@Component ( {
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: [ './profile.component.css' ]
-} )
+@Component({
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"]
+})
 export class ProfileComponent implements OnInit {
   posts: Array<userPost>;
   userDetails: userDetails;
 
   private comments: Array<PostComments>;
 
-  constructor ( private api: ApiService ) {
+  constructor(private api: ApiService) {}
+
+  ngOnInit() {
+    this.userDetails = JSON.parse(localStorage.getItem("loggedInUser"));
+    console.log(this.userDetails);
+    this.api.getComments().subscribe(
+      data => {
+        console.log("got comments");
+        this.comments = data;
+        this.getposts();
+      },
+      error => console.log(error)
+    );
   }
 
-  ngOnInit () {
-    this.userDetails = JSON.parse ( localStorage.getItem ( "loggedInUser" ) );
-    console.log(this.userDetails)
-    this.api.getComments().subscribe(data => {
-      console.log('got comments');
-      this.comments = data;
-      this.getposts();
-    }, error => console.log(error));
-  }
-
-  private getposts(){
-    this.api.getPosts ( this.userDetails.id ).subscribe (
-      ( data ) => {
+  private getposts() {
+    this.api.getPosts(this.userDetails.id).subscribe(
+      data => {
         this.posts = data;
-        this.posts.forEach( post => {
+        this.posts.forEach(post => {
           post.comments = this.getSavedComments(post.id);
         });
-      }, error => {
-        console.log ( error );
+      },
+      error => {
+        console.log(error);
       }
     );
   }
 
-  private getSavedComments ( postId: number ) {
+  private getSavedComments(postId: number) {
     const response: Array<PostComments> = [];
-    this.comments.forEach ( comment => {
+    this.comments.forEach(comment => {
       if (comment.postId == postId) {
-        response.push ( comment );
+        response.push(comment);
       }
-    } );
+    });
     return response;
   }
 }
